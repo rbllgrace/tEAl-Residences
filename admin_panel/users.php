5fe354f232a03a4f37c3ce399cce4b339abd7808
Ericson Palisoc <palisocericson87@gmail.com>
2023-12-19 00:19:03 +0800

    search user bug fixed

--- admin_panel/users.php
+++ admin_panel/users.php
@@ -41,18 +41,16 @@ admin_login();
 
             </tbody>
         </table>
-        <div id="notFoundMessage" style="display: none; color: red;" class="text-center">User Not Found</div>
+        <div id="notFoundMessage" style="display: none; color: red;" class="text-center">User Not Found!</div>
     </div>
 
     <!-- Edit User Modal -->
-    <div class="modal fade modal_add_user" id="editUserModal" data-bs-backdrop="static" data-bs-keyboard="false"
-        tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
+    <div class="modal fade modal_add_user" id="editUserModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="editUserModalLabel">Edit User</h1>
-                    <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                    <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body modal_content_edit_user_container">
                 </div>
@@ -62,141 +60,141 @@ admin_login();
     </div>
 
     <script>
-    window.onload = function() {
-        get_users()
-    }
+        window.onload = function() {
+            get_users()
+        }
 
-    function alert(type, msg) {
+        function alert(type, msg) {
 
-        let base_class = (type == 'success') ? 'alert-success' : 'alert-danger'
-        let element = document.createElement('div')
-        element.innerHTML = `<div class="alert ${base_class} alert-dismissible fade show custom_alert" role="alert">
+            let base_class = (type == 'success') ? 'alert-success' : 'alert-danger'
+            let element = document.createElement('div')
+            element.innerHTML = `<div class="alert ${base_class} alert-dismissible fade show custom_alert" role="alert">
 ${msg}
 <button type="button" class="btn-close shadow-none" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>`
-        document.body.append(element)
+            document.body.append(element)
 
-        // Use setTimeout to remove the alert after the specified duration
-        setTimeout(function() {
-            element.remove();
-        }, 3000);
-    }
-
-    function validateInput(input) {
-        var value = parseFloat(input.value);
-
-        if (isNaN(value) || value < -1) {
-            input.value = 0;
-        } else if (value === 0) {
-            input.value = 0;
-        } else if (value > 1) {
-            input.value = 1;
-        }
-    }
-
-    function get_users() {
-
-        let xhr = new XMLHttpRequest()
-        xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-        xhr.onload = function() {
-            const table_body = document.querySelector('.table_body');
-            table_body.innerHTML = this.responseText
-        }
-        xhr.send('get_users')
-    }
-
-    const modal_content_edit_user_container = document.querySelector('.modal_content_edit_user_container');
-
-    function get_single_user_with_id(user_id) {
-        let xhr = new XMLHttpRequest()
-        xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-        xhr.onload = function() {
-
-            modal_content_edit_user_container.innerHTML = this.responseText
+            // Use setTimeout to remove the alert after the specified duration
+            setTimeout(function() {
+                element.remove();
+            }, 3000);
         }
 
-        xhr.send('user_id=' + user_id + '&get_single_user_with_id')
-    }
+        function validateInput(input) {
+            var value = parseFloat(input.value);
 
-    function edit_user_by_id(user_id) {
-        const name_inp_user = document.querySelector('.name_inp_user');
-        const is_verified_inp_user = document.querySelector('.is_verified_inp_user');
-
-        let xhr = new XMLHttpRequest()
-        xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-        xhr.onload = function() {
-
-            let my_modal = document.querySelector('.modal_add_user')
-            let modal = bootstrap.Modal.getInstance(my_modal)
-            modal.hide()
-
-            if (this.responseText == 1) {
-                alert('success', 'User Updated!')
-                setTimeout(() => {
-                    window.location.reload();
-                }, 500);
-            } else {
-                alert('error', 'No Changes Made!')
+            if (isNaN(value) || value < -1) {
+                input.value = 0;
+            } else if (value === 0) {
+                input.value = 0;
+            } else if (value > 1) {
+                input.value = 1;
             }
         }
 
-        xhr.send('name_inp_user=' + name_inp_user.value + '&is_verified_inp_user=' + is_verified_inp_user.value +
-            '&user_id=' + user_id +
-            '&edit_user_by_id')
+        function get_users() {
 
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
-    }
-
-    function delete_single_user(user_id) {
-        let xhr = new XMLHttpRequest()
-        xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-        xhr.onload = function() {
-            if (this.responseText == 1) {
-                alert('success', 'Removed!')
-                setTimeout(() => {
-                    window.location.reload();
-                }, 500);
-            } else {
-                alert('error', 'Something went wrong!')
+            xhr.onload = function() {
+                const table_body = document.querySelector('.table_body');
+                table_body.innerHTML = this.responseText
             }
+            xhr.send('get_users')
         }
 
-        xhr.send('user_id=' + user_id + '&delete_single_user')
-    }
+        const modal_content_edit_user_container = document.querySelector('.modal_content_edit_user_container');
 
+        function get_single_user_with_id(user_id) {
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
-    function searchUsers() {
+            xhr.onload = function() {
 
-        let input = document.getElementById('userSearchInput').value.toLowerCase();
-        let rows = document.querySelectorAll('.table_body tr');
-
-        let userFound = false;
-
-        rows.forEach(function(row) {
-            let name = row.querySelector('td:nth-child(2)').textContent.toLowerCase();
-            if (name.includes(input)) {
-                row.style.display = '';
-                userFound = true;
-            } else {
-                row.style.display = 'none';
+                modal_content_edit_user_container.innerHTML = this.responseText
             }
-        });
 
-        let notFoundMessageElement = document.getElementById('notFoundMessage');
-        if (!userFound) {
-            notFoundMessageElement.style.display = 'block';
-        } else {
-            notFoundMessageElement.style.display = 'none';
+            xhr.send('user_id=' + user_id + '&get_single_user_with_id')
         }
-    }
+
+        function edit_user_by_id(user_id) {
+            const name_inp_user = document.querySelector('.name_inp_user');
+            const is_verified_inp_user = document.querySelector('.is_verified_inp_user');
+
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+            xhr.onload = function() {
+
+                let my_modal = document.querySelector('.modal_add_user')
+                let modal = bootstrap.Modal.getInstance(my_modal)
+                modal.hide()
+
+                if (this.responseText == 1) {
+                    alert('success', 'User Updated!')
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 500);
+                } else {
+                    alert('error', 'No Changes Made!')
+                }
+            }
+
+            xhr.send('name_inp_user=' + name_inp_user.value + '&is_verified_inp_user=' + is_verified_inp_user.value +
+                '&user_id=' + user_id +
+                '&edit_user_by_id')
+
+
+        }
+
+        function delete_single_user(user_id) {
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', 'http://localhost/teal-residences/admin_panel/ajax/settings_crud.php', true)
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+            xhr.onload = function() {
+                if (this.responseText == 1) {
+                    alert('success', 'Removed!')
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 500);
+                } else {
+                    alert('error', 'Something went wrong!')
+                }
+            }
+
+            xhr.send('user_id=' + user_id + '&delete_single_user')
+        }
+
+
+        function searchUsers() {
+
+            let input = document.getElementById('userSearchInput').value.toLowerCase();
+            let rows = document.querySelectorAll('.table_body tr');
+
+            let userFound = false;
+
+            rows.forEach(function(row) {
+                let name = row.querySelector('td:nth-child(2)').textContent.toLowerCase();
+                if (name.includes(input)) {
+                    row.style.display = '';
+                    userFound = true;
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+
+            let notFoundMessageElement = document.getElementById('notFoundMessage');
+            if (!userFound) {
+                notFoundMessageElement.style.display = 'block';
+            } else {
+                notFoundMessageElement.style.display = 'none';
+            }
+        }
     </script>
 
 
